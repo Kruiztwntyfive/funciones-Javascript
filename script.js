d65e6c381dc85580fb49df4903962cd066c71f64
JuniorGino <ginomaranha@gmail.com>
2025-11-21 10:16:56 +0100

    parte del codigo

--- script.js
+++ script.js
@@ -1,3 +1,41 @@
- function saludarDemo() {
-                    console.log("Hola desde javascript!");  
-                }
+ 
+ 
+ /*ESTO ES UNA FUNCIÓN DE EJEMPLO*/
+ 
+        function saludarDemo() {
+        console.log("Hola desde javascript!"); 
+    }
+       
+/*ESTO ES UN EJEMPLO DE DECLARACION*/
+
+      let nombre="Anna";
+        function mostrarNombreConsola() {
+        console.log("Hola", nombre);
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+    
+  /*ESTO ES UNA FUNCIÓN PARA LIMPIAR LA CONSOLA*/  
+        function limpiarConsola() {
+        console.clear();
+    }
